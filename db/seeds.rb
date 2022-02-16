fd7ca489623024d8310a3f32dfc095dc25da3772
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-16 11:52:16 +0000

    Added Restaurant model and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all restaurants'
+
+Restaurant.destroy_all
+
+puts 'Creating 50 restaurants'
+
+50.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+                    category: Faker::Restaurant.type,
+                    address: Faker::Address.street_address,
+                    phone_number: Faker::PhoneNumber.cell_phone)
+end
+
+puts "Created #{Restaurant.count} restaurants"
